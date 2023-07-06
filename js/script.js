167ffb239ccf660c105c36ce85a1c24c7e179254
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-06 06:43:09 +0000

    コメント

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
-$('#tab-contents .tab[id !="tab"]').hide();
-$('#tab-menu a').on('click', function(event){
- $("#tab-contents .tab").hide();
-  $("#tab-mwnu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("heef")).show();
-  event.preventDefault();
+const API_KEY = "ddfa8741c65284f1b39f77da6cc506d2"
+$(function(){
+  $('#btn').on('click',function(){
+   $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+    dataType : 'jsonp',
+   }).done(function(date){
+    //通信成功
+    //位置
+    $('#place').text(data.name);
+    // 最高気温
+    $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+   }).fail(function(data){
+//通信失敗
+      alert('通信に失敗しました。');  
+   });
+  });
 });
